11322f381e5c64b90514ec0ecbc2095868bd2aa4
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-04-20 18:00:39 +0100

    jquery code working perfectly

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 var tasks = [];
 
 function addTask() {
-  var task = document.getElementById("taskName").value;
-  var date = document.getElementById("date").value;
-  var time = document.getElementById("time").value;
-  var priority = document.getElementById("priority").value;
-  var taskDescription = document.getElementById("desp").value;
+  var task = $("#taskName").val();
+  var date = $("#date").val();
+  var time = $("#time").val();
+  var priority = $("#priority").val();
+  var taskDescription = $("#desp").val();
 
   if (isEmpty(task, date, time, priority, taskDescription)) {
-    document.getElementById("alertDanger").innerHTML = `
-            <p class="alertDanger">
-            Please add a task
-            </p>
-        `;
-        setTimeout(()=>{
-          document.getElementById("alertDanger").innerHTML = ""
-        }, 2000)
+    $("#alertDanger").html(`
+        <p class="alertDanger">
+        Please add a task
+        </p>
+    `).fadeIn("slow");
+
+    setTimeout(() => {
+      $("#alertDanger").fadeOut("slow");
+    }, 2000);
   } else {
     var taskObject = {
       task: task,
@@ -28,23 +29,18 @@ function addTask() {
     tasks.push(taskObject);
     localStorage.setItem("tasks", JSON.stringify(tasks));
 
-    document.getElementById("taskName").value = "";
-    document.getElementById("date").value = "";
-    document.getElementById("time").value = "";
-    document.getElementById("priority").value = "";
-    document.getElementById("desp").value = "";
+    $("#taskName, #date, #time, #priority, #desp").val("");
 
-    document.getElementById("alertSuccess").innerHTML = `
-            <p class="alertSuccess">
-            Task added to list successfully
-            </p>
-        `;
-        setTimeout(()=>{
-          document.getElementById("alertSuccess").innerHTML = ""
-        }, 1000)
+    $("#alertSuccess").html(`
+        <p class="alertSuccess">
+        Task added to list successfully
+        </p>
+    `).fadeIn("slow");
 
     setTimeout(() => {
-      window.location.href = "task.html";
+      $("#alertSuccess").fadeOut("slow", function() {
+        window.location.href = "task.html";
+      });
     }, 2000);
 
     console.log(tasks);
@@ -60,13 +56,13 @@ function isEmpty(...values) {
 var tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 function displayTasks() {
-  var taskTable = document.getElementById("taskTable");
+  var taskTable = $("#taskTable");
 
-  taskTable.innerHTML = "";
+  taskTable.html("");
 
   if (tasks) {
     tasks.forEach((task, index) => {
-      taskTable.innerHTML += `
+      taskTable.append(`
             <tr>
             <td>${task.task}</td>
             <td>${task.date}</td>
@@ -80,7 +76,7 @@ function displayTasks() {
             <button class="edit" onclick="editTask(${index})">Edit</button>
             </td>                 
             </tr>
-        `;
+        `);
     });
   }
 }
@@ -89,24 +85,27 @@ displayTasks();
 function deleteTask(index) {
   tasks.splice(index, 1);
   localStorage.setItem("tasks", JSON.stringify(tasks));
-  taskTable.innerHTML = "";
+  $("#taskTable").html("");
   displayTasks();
 }
 
 function editTask(index) {
-  var taskTable = document.getElementById("taskTable");
-  const task = prompt("Enter the new task name:");
-  const date = prompt("Enter the new date:");
-  const time = prompt("Enter the new time:");
-  const priority = prompt("Enter the new priority:");
-  const taskDescription = prompt("Enter the new task description:");
+  var task = prompt("Enter the new task name:");
+  var date = prompt("Enter the new date:");
+  var time = prompt("Enter the new time:");
+  var priority = prompt("Enter the new priority:");
+  var taskDescription = prompt("Enter the new task description:");
 
   if (isEmpty(task, date, time, priority, taskDescription)) {
-    document.getElementById("alertDanger").innerHTML = `
+    $("#alertDanger").html(`
             <p class="alertDanger">
             Please add a task
             </p>
-        `;
+        `).fadeIn("slow");
+
+    setTimeout(() => {
+      $("#alertDanger").fadeOut("slow");
+    }, 2000);
   } else {
     tasks[index].task = task;
     tasks[index].date = date;
@@ -114,7 +113,7 @@ function editTask(index) {
     tasks[index].priority = priority;
     tasks[index].taskDescription = taskDescription;
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    taskTable.innerHTML = "";
+    $("#taskTable").html("");
     displayTasks();
   }
 }
